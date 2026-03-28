714ead86c8f85c9f8dafcdc9eb9782fd397255c3
pooja <your-email@example.com>
2026-03-28 16:26:07 +0530

    Updated Jenkins pipeline

--- disable_ssh.py
+++ disable_ssh.py
@@ -2,7 +2,7 @@ import boto3
 
 ec2 = boto3.client('ec2', region_name='ap-south-1')
 
-SECURITY_GROUP_ID = 'sg-0b92f94bb54aefc8e'   # replace
+SECURITY_GROUP_ID = 'sg-0b92f94bb54aefc8e'   # replace(qatest-ec2-sg-name)
 
 def remove_ssh():
     try:
